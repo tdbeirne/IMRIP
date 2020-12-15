74bcc9ffd91c8f476c790bb5000e4cf593877302
Thomas <tdbeirne@yahoo.com>
2020-12-15 06:40:37 -0600

    first neural net that performs somewhat well

--- predict.py
+++ predict.py
@@ -14,122 +14,60 @@ class Net(nn.Module):
         #However, I'm going with a much smaller net to see how it performs
 
         #encode
-        self.conv32_1 = nn.Conv3d(in_channels=4, out_channels=32, kernel_size=(2,2,2), stride=1, padding=1)
-        self.batch_norm32_1 = nn.BatchNorm3d(num_features=32)
-        # self.conv32_2 = nn.Conv3d(in_channels=32, out_channels=32, kernel_size=(2,2,2), stride=1, padding=1)
-        # self.batch_norm32_2 = nn.BatchNorm3d(num_features=32)
+        self.conv_1 = nn.Conv3d(in_channels=4, out_channels=8, kernel_size=(2,2,2), stride=2, padding=0)
+        self.norm_1 = nn.BatchNorm3d(num_features=8)
 
-        self.pool32 = nn.MaxPool3d(kernel_size=(2,2,2), return_indices=True)
+        self.conv_2 = nn.Conv3d(in_channels=8, out_channels=16, kernel_size=(2,2,2), stride=2, padding=0)
+        self.norm_2 = nn.BatchNorm3d(num_features=16)
 
-        self.conv64_1 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=(2,2,2), stride=1, padding=1)
-        self.batch_norm64_1 = nn.BatchNorm3d(num_features=64)
-        # self.conv64_2 = nn.Conv3d(in_channels=64, out_channels=64, kernel_size=(2,2,2), stride=1, padding=1)
-        # self.batch_norm64_2 = nn.BatchNorm3d(num_features=64)
+        self.conv_3 = nn.Conv3d(in_channels=16, out_channels=32, kernel_size=(2,2,2), stride=2, padding=0)
+        self.norm_3 = nn.BatchNorm3d(num_features=32)
 
-        self.pool64 = nn.MaxPool3d(kernel_size=(2,2,2), return_indices=True)
+        self.conv_4 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=(2,2,2), stride=2, padding=0)
+        self.norm_4 = nn.BatchNorm3d(num_features=64)
 
-        self.conv128_1 = nn.Conv3d(in_channels=64, out_channels=128, kernel_size=(2,2,2), stride=1, padding=1)
-        self.batch_norm128_1 = nn.BatchNorm3d(num_features=128)
-        # self.conv128_2 = nn.Conv3d(in_channels=128, out_channels=128, kernel_size=(2,2,2), stride=1, padding=1)
-        # self.batch_norm128_2 = nn.BatchNorm3d(num_features=128)
+        # self.conv_5 = nn.Conv3d(in_channels=64, out_channels=128, kernel_size=(2,2,2), stride=2, padding=0)
+        # self.norm_5 = nn.BatchNorm3d(num_features=128)
+        #
+        # self.conv_6 = nn.Conv3d(in_channels=128, out_channels=256, kernel_size=(2,2,2), stride=2, padding=0)
+        # self.norm_6 = nn.BatchNorm3d(num_features=256)
+        #
+        # self.unconv_1 = nn.ConvTranspose3d(in_channels=256, out_channels=128, kernel_size=(2,2,2), stride=2, padding=0)
+        # self.unnorm_1 = nn.BatchNorm3d(num_features=128)
+        #
+        # self.unconv_2 = nn.ConvTranspose3d(in_channels=128, out_channels=64, kernel_size=(2,2,2), stride=2, padding=0)
+        # self.unnorm_2 = nn.BatchNorm3d(num_features=64)
 
-        self.pool128 = nn.MaxPool3d(kernel_size=(2,2,2), return_indices=True)
+        self.unconv_3 = nn.ConvTranspose3d(in_channels=64, out_channels=32, kernel_size=(2,2,2), stride=2, padding=0)
+        self.unnorm_3 = nn.BatchNorm3d(num_features=32)
 
-        #decode
-        self.unpool128 = nn.MaxUnpool3d(kernel_size=(2,2,2), stride=2)
+        self.unconv_4 = nn.ConvTranspose3d(in_channels=32, out_channels=16, kernel_size=(2,2,2), stride=2, padding=0)
+        self.unnorm_4 = nn.BatchNorm3d(num_features=16)
 
-        self.unconv128_1 = nn.ConvTranspose3d(in_channels=128, out_channels=64, kernel_size=(1,1,1), stride=1, padding=0)
-        self.unbatch_norm128_1 = nn.BatchNorm3d(num_features=64)
-        # self.unconv128_2 = nn.ConvTranspose3d(in_channels=128, out_channels=64, kernel_size=(2,2,2), stride=1, padding=1)
-        # self.unbatch_norm128_2 = nn.BatchNorm3d(num_features=64)
+        self.unconv_5 = nn.ConvTranspose3d(in_channels=16, out_channels=8, kernel_size=(2,2,2), stride=2, padding=0)
+        self.unnorm_5 = nn.BatchNorm3d(num_features=8)
 
-        self.unpool64 = nn.MaxUnpool3d(kernel_size=(2,2,2), stride=2)
-
-        self.unconv64_1 = nn.ConvTranspose3d(in_channels=64, out_channels=32, kernel_size=(1,1,1), stride=1, padding=0)
-        self.unbatch_norm64_1 = nn.BatchNorm3d(num_features=32)
-        # self.unconv64_2 = nn.ConvTranspose3d(in_channels=64, out_channels=32, kernel_size=(2,2,2), stride=1, padding=1)
-        # self.unbatch_norm64_2 = nn.BatchNorm3d(num_features=32)
-
-        self.unpool32 = nn.MaxUnpool3d(kernel_size=(2,2,2), stride=2)
-
-        self.unconv32_1 = nn.ConvTranspose3d(in_channels=32, out_channels=4, kernel_size=(1,1,1), stride=1, padding=0)
-        # self.unbatch_norm32_1 = nn.BatchNorm3d(num_features=32)
-        # self.unconv32_2 = nn.ConvTranspose3d(in_channels=32, out_channels=4, kernel_size=(2,2,2), stride=2, padding=1)
+        self.unconv_6 = nn.ConvTranspose3d(in_channels=8, out_channels=4, kernel_size=(2,2,2), stride=2, padding=0)
 
     def forward(self, x):
         #encode
-        size_0 = x.size()
-        #print("SIZE 0: {}".format(size_0))
-
-        x = F.relu(self.batch_norm32_1(self.conv32_1(x)))
-        # x = F.relu(self.batch_norm32_2(self.conv32_2(x)))
-        x, indices_32 = self.pool32(x)
-
-
-        #print("X_32: {}".format(x.shape))
-        #print("X_32 INDICES: {}".format(indices_32.shape))
-
-
-        size_1 = x.size()
-        #print("SIZE 1: {}".format(size_1))
-
-        x = F.relu(self.batch_norm64_1(self.conv64_1(x)))
-        # x = F.relu(self.batch_norm64_2(self.conv64_2(x)))
-        x, indices_64 = self.pool64(x)
-
-        #print("X_64: {}".format(x.shape))
-        #print("X_64 INDICES: {}".format(indices_64.shape))
-
-        size_2 = x.size()
-        #print("SIZE 2: {}".format(size_2))
-
-        x = F.relu(self.batch_norm128_1(self.conv128_1(x)))
-        # x = F.relu(self.batch_norm128_2(self.conv128_2(x)))
-        x, indices_128 = self.pool128(x)
-
-
-        #print("X_128: {}".format(x.shape))
-        #print("X_128 INDICES: {}".format(indices_128.shape))
-
-        #decode
-        x = self.unpool128(x, indices_128, output_size=size_2)
-
-        #print("X_128_AFTER: {}".format(x.shape))
-        #print("X_128_AFTER INDICES: {}".format(indices_128.shape))
-
-        x = F.relu(self.unbatch_norm128_1(self.unconv128_1(x)))
-        # x = F.relu(self.unbatch_norm128_2(self.unconv128_2(x)))
-
-        #print("X_128_AFTER_RELU: {}".format(x.shape))
-
-        x = self.unpool64(x, indices_64, output_size=size_1)
-
-        #print("X_64_AFTER: {}".format(x.shape))
-        #print("X_64_AFTER INDICES: {}".format(indices_64.shape))
-
-        x = F.relu(self.unbatch_norm64_1(self.unconv64_1(x)))
-        # x = F.relu(self.unbatch_norm64_2(self.unconv64_2(x)))
-
-        #print("X_64_AFTER_RELU: {}".format(x.shape))
-
-        x = self.unpool32(x, indices_32, output_size=size_0)
-
-        #print("X_32_AFTER: {}".format(x.shape))
-        #print("X_32_AFTER INDICES: {}".format(indices_32.shape))
-
-        # x = F.relu(self.unbatch_norm32_1(self.unconv32_1(x)))
-        x = self.unconv32_1(x)
-
-        #print("X_32_AFTER_RELU: {}".format(x.shape))
-
-        #print(x.shape)
+        x = F.relu(self.norm_1(self.conv_1(x)))
+        x = F.relu(self.norm_2(self.conv_2(x)))
+        x = F.relu(self.norm_3(self.conv_3(x)))
+        x = F.relu(self.norm_4(self.conv_4(x)))
+        # x = F.relu(self.norm_5(self.conv_5(x)))
+        # x = F.relu(self.norm_6(self.conv_6(x)))
+        # x = F.relu(self.unnorm_1(self.unconv_1(x)))
+        # x = F.relu(self.unnorm_2(self.unconv_2(x)))
+        x = F.relu(self.unnorm_3(self.unconv_3(x)))
+        x = F.relu(self.unnorm_4(self.unconv_4(x)))
+        x = F.relu(self.unnorm_5(self.unconv_5(x)))
+        x = self.unconv_6(x)
 
         return x
 
-
-
 net = Net()
-net.load_state_dict(torch.load("./model_2.pth"))
+net.load_state_dict(torch.load("./model_6.pth"))
 print("Number of parameters {}".format(sum([p.numel() for p in net.parameters()])))
 
 data_files = sorted(glob.glob("./data/validation/*imgs.npy"))
@@ -139,14 +77,19 @@ data_tensors = []
 for data_file in data_files:
     print(data_file)
 
-    id = data_file.split("_")[0][-3:]
+    id = data_file[-12:].split("_")[0][-3:]
 
     data_mat = np.load(data_file)
     data_tensor = torch.from_numpy(data_mat)
     data_tensor = torch.unsqueeze(data_tensor, 0)
 
     output = net(data_tensor)
+    #print(output.size())
+    #print(data_tensor.size())
+    output = F.interpolate(output, size=data_tensor.size()[2:], mode="trilinear")
     output = output.squeeze()
+
+    #print(output.size())
 
     _, prediction = torch.max(output, dim=0, keepdim=False)
     prediction_as_mat = prediction.numpy()
--- train.py
+++ train.py
@@ -27,131 +27,73 @@ class Net(nn.Module):
         #However, I'm going with a much smaller net to see how it performs
 
         #encode
-        self.conv32_1 = nn.Conv3d(in_channels=4, out_channels=32, kernel_size=(2,2,2), stride=1, padding=1)
-        self.batch_norm32_1 = nn.BatchNorm3d(num_features=32)
-        # self.conv32_2 = nn.Conv3d(in_channels=32, out_channels=32, kernel_size=(2,2,2), stride=1, padding=1)
-        # self.batch_norm32_2 = nn.BatchNorm3d(num_features=32)
+        self.conv_1 = nn.Conv3d(in_channels=4, out_channels=8, kernel_size=(2,2,2), stride=2, padding=0)
+        self.norm_1 = nn.BatchNorm3d(num_features=8)
 
-        self.pool32 = nn.MaxPool3d(kernel_size=(2,2,2), return_indices=True)
+        self.conv_2 = nn.Conv3d(in_channels=8, out_channels=16, kernel_size=(2,2,2), stride=2, padding=0)
+        self.norm_2 = nn.BatchNorm3d(num_features=16)
 
-        self.conv64_1 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=(2,2,2), stride=1, padding=1)
-        self.batch_norm64_1 = nn.BatchNorm3d(num_features=64)
-        # self.conv64_2 = nn.Conv3d(in_channels=64, out_channels=64, kernel_size=(2,2,2), stride=1, padding=1)
-        # self.batch_norm64_2 = nn.BatchNorm3d(num_features=64)
+        self.conv_3 = nn.Conv3d(in_channels=16, out_channels=32, kernel_size=(2,2,2), stride=2, padding=0)
+        self.norm_3 = nn.BatchNorm3d(num_features=32)
 
-        self.pool64 = nn.MaxPool3d(kernel_size=(2,2,2), return_indices=True)
+        self.conv_4 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=(2,2,2), stride=2, padding=0)
+        self.norm_4 = nn.BatchNorm3d(num_features=64)
 
-        self.conv128_1 = nn.Conv3d(in_channels=64, out_channels=128, kernel_size=(2,2,2), stride=1, padding=1)
-        self.batch_norm128_1 = nn.BatchNorm3d(num_features=128)
-        # self.conv128_2 = nn.Conv3d(in_channels=128, out_channels=128, kernel_size=(2,2,2), stride=1, padding=1)
-        # self.batch_norm128_2 = nn.BatchNorm3d(num_features=128)
+        # self.conv_5 = nn.Conv3d(in_channels=64, out_channels=128, kernel_size=(2,2,2), stride=2, padding=0)
+        # self.norm_5 = nn.BatchNorm3d(num_features=128)
+        #
+        # self.conv_6 = nn.Conv3d(in_channels=128, out_channels=256, kernel_size=(2,2,2), stride=2, padding=0)
+        # self.norm_6 = nn.BatchNorm3d(num_features=256)
+        #
+        # self.unconv_1 = nn.ConvTranspose3d(in_channels=256, out_channels=128, kernel_size=(2,2,2), stride=2, padding=0)
+        # self.unnorm_1 = nn.BatchNorm3d(num_features=128)
+        #
+        # self.unconv_2 = nn.ConvTranspose3d(in_channels=128, out_channels=64, kernel_size=(2,2,2), stride=2, padding=0)
+        # self.unnorm_2 = nn.BatchNorm3d(num_features=64)
 
-        self.pool128 = nn.MaxPool3d(kernel_size=(2,2,2), return_indices=True)
+        self.unconv_3 = nn.ConvTranspose3d(in_channels=64, out_channels=32, kernel_size=(2,2,2), stride=2, padding=0)
+        self.unnorm_3 = nn.BatchNorm3d(num_features=32)
 
-        #decode
-        self.unpool128 = nn.MaxUnpool3d(kernel_size=(2,2,2), stride=2)
+        self.unconv_4 = nn.ConvTranspose3d(in_channels=32, out_channels=16, kernel_size=(2,2,2), stride=2, padding=0)
+        self.unnorm_4 = nn.BatchNorm3d(num_features=16)
 
-        self.unconv128_1 = nn.ConvTranspose3d(in_channels=128, out_channels=64, kernel_size=(1,1,1), stride=1, padding=0)
-        self.unbatch_norm128_1 = nn.BatchNorm3d(num_features=64)
-        # self.unconv128_2 = nn.ConvTranspose3d(in_channels=128, out_channels=64, kernel_size=(2,2,2), stride=1, padding=1)
-        # self.unbatch_norm128_2 = nn.BatchNorm3d(num_features=64)
+        self.unconv_5 = nn.ConvTranspose3d(in_channels=16, out_channels=8, kernel_size=(2,2,2), stride=2, padding=0)
+        self.unnorm_5 = nn.BatchNorm3d(num_features=8)
 
-        self.unpool64 = nn.MaxUnpool3d(kernel_size=(2,2,2), stride=2)
-
-        self.unconv64_1 = nn.ConvTranspose3d(in_channels=64, out_channels=32, kernel_size=(1,1,1), stride=1, padding=0)
-        self.unbatch_norm64_1 = nn.BatchNorm3d(num_features=32)
-        # self.unconv64_2 = nn.ConvTranspose3d(in_channels=64, out_channels=32, kernel_size=(2,2,2), stride=1, padding=1)
-        # self.unbatch_norm64_2 = nn.BatchNorm3d(num_features=32)
-
-        self.unpool32 = nn.MaxUnpool3d(kernel_size=(2,2,2), stride=2)
-
-        self.unconv32_1 = nn.ConvTranspose3d(in_channels=32, out_channels=4, kernel_size=(1,1,1), stride=1, padding=0)
-        # self.unbatch_norm32_1 = nn.BatchNorm3d(num_features=32)
-        # self.unconv32_2 = nn.ConvTranspose3d(in_channels=32, out_channels=4, kernel_size=(2,2,2), stride=2, padding=1)
+        self.unconv_6 = nn.ConvTranspose3d(in_channels=8, out_channels=4, kernel_size=(2,2,2), stride=2, padding=0)
 
     def forward(self, x):
         #encode
-        size_0 = x.size()
-        #print("SIZE 0: {}".format(size_0))
-
-        x = F.relu(self.batch_norm32_1(self.conv32_1(x)))
-        # x = F.relu(self.batch_norm32_2(self.conv32_2(x)))
-        x, indices_32 = self.pool32(x)
-
-
-        #print("X_32: {}".format(x.shape))
-        #print("X_32 INDICES: {}".format(indices_32.shape))
-
-
-        size_1 = x.size()
-        #print("SIZE 1: {}".format(size_1))
-
-        x = F.relu(self.batch_norm64_1(self.conv64_1(x)))
-        # x = F.relu(self.batch_norm64_2(self.conv64_2(x)))
-        x, indices_64 = self.pool64(x)
-
-        #print("X_64: {}".format(x.shape))
-        #print("X_64 INDICES: {}".format(indices_64.shape))
-
-        size_2 = x.size()
-        #print("SIZE 2: {}".format(size_2))
-
-        x = F.relu(self.batch_norm128_1(self.conv128_1(x)))
-        # x = F.relu(self.batch_norm128_2(self.conv128_2(x)))
-        x, indices_128 = self.pool128(x)
-
-
-        #print("X_128: {}".format(x.shape))
-        #print("X_128 INDICES: {}".format(indices_128.shape))
-
-        #decode
-        x = self.unpool128(x, indices_128, output_size=size_2)
-
-        #print("X_128_AFTER: {}".format(x.shape))
-        #print("X_128_AFTER INDICES: {}".format(indices_128.shape))
-
-        x = F.relu(self.unbatch_norm128_1(self.unconv128_1(x)))
-        # x = F.relu(self.unbatch_norm128_2(self.unconv128_2(x)))
-
-        #print("X_128_AFTER_RELU: {}".format(x.shape))
-
-        x = self.unpool64(x, indices_64, output_size=size_1)
-
-        #print("X_64_AFTER: {}".format(x.shape))
-        #print("X_64_AFTER INDICES: {}".format(indices_64.shape))
-
-        x = F.relu(self.unbatch_norm64_1(self.unconv64_1(x)))
-        # x = F.relu(self.unbatch_norm64_2(self.unconv64_2(x)))
-
-        #print("X_64_AFTER_RELU: {}".format(x.shape))
-
-        x = self.unpool32(x, indices_32, output_size=size_0)
-
-        #print("X_32_AFTER: {}".format(x.shape))
-        #print("X_32_AFTER INDICES: {}".format(indices_32.shape))
-
-        # x = F.relu(self.unbatch_norm32_1(self.unconv32_1(x)))
-        x = self.unconv32_1(x)
-
-        #print("X_32_AFTER_RELU: {}".format(x.shape))
-
-        #print(x.shape)
+        x = F.relu(self.norm_1(self.conv_1(x)))
+        x = F.relu(self.norm_2(self.conv_2(x)))
+        x = F.relu(self.norm_3(self.conv_3(x)))
+        x = F.relu(self.norm_4(self.conv_4(x)))
+        # x = F.relu(self.norm_5(self.conv_5(x)))
+        # x = F.relu(self.norm_6(self.conv_6(x)))
+        # x = F.relu(self.unnorm_1(self.unconv_1(x)))
+        # x = F.relu(self.unnorm_2(self.unconv_2(x)))
+        x = F.relu(self.unnorm_3(self.unconv_3(x)))
+        x = F.relu(self.unnorm_4(self.unconv_4(x)))
+        x = F.relu(self.unnorm_5(self.unconv_5(x)))
+        x = self.unconv_6(x)
 
         return x
 
 #check if CUDA is available
 if torch.cuda.is_available():
     print("Using CUDA")
+    torch.cuda.empty_cache()
 
 
 net = Net()
 
 #load onto GPU
 net = net.cuda()
+print(torch.cuda.memory_summary(device=0))
 
 #[1.0, 131.4, 53.3, 163.1]
-criterion = nn.CrossEntropyLoss(weight=torch.FloatTensor([1.0, 300, 300, 300]).cuda())
-optimizer = optim.SGD(net.parameters(), lr=0.005, momentum=0.9)
+criterion = nn.CrossEntropyLoss(weight=torch.FloatTensor([1.0, 300.0, 300.0, 300.0]).cuda())
+optimizer = optim.Adam(net.parameters(), lr=0.010)
 
 #load data
 data_files = sorted(glob.glob("./data/train/*imgs.npy"))
@@ -189,46 +131,54 @@ label_stack = torch.cat(label_tensors, dim=0)
 label_stack = torch.squeeze(label_stack, dim=1)
 label_stack = label_stack.long()
 
+batch_size = 30
+
 #create dataset out of stacks
 dataset = TensorDataset(data_stack, label_stack)
-loader = DataLoader(dataset=dataset, batch_size=4, shuffle=True)
+loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
 
 #print(label_stack.shape)
 #print(data_stack.shape)
 
-batch_size = 10
-num_epochs = 20
-losses = []
+print("INITIAL MEMORY USAGE: {}".format(torch.cuda.memory_summary(device=0)))
+
 
 #Resource I used for building a simple model trainer
 #https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+num_epochs = 40
+
+print("Number of parameters: {}".format(sum([p.numel() for p in net.parameters()])))
 
 for epoch in range(num_epochs):
     epoch_start_time = time.time()
     running_loss = 0
+    batch_num = 0
     for x_batch, y_batch in loader:
+        #print(x_batch.size())
+        #print(y_batch.size())
+
+        batch_num += 1
+        #print("BATCH {} MEMORY USAGE: {}".format(batch_num, torch.cuda.memory_summary(device=0)))
         x_batch,y_batch = x_batch.cuda(), y_batch.cuda()
-
-
-        #print(x_batch.shape)
-        #print(y_batch.shape)
 
         batch_start_time = time.time()
         #zero out gradients because we are working on a new batch
         optimizer.zero_grad()
 
         outputs = net(x_batch)
-        #print(outputs.shape)
+        #print(outputs.size())
+        #print(outputs.get_device())
+
         loss = criterion(outputs, y_batch)
         loss.backward()
         optimizer.step()
 
         running_loss += loss.item()
     print("COMPLETED EPOCH {} IN {} SECONDS".format(epoch, time.time() -  epoch_start_time))
-    print("EPOCH AVERAGE LOSS: {}".format(running_loss / 12.75))
+    print("EPOCH AVERAGE LOSS: {}".format(running_loss / (204 / batch_size)))
 
 
-output_path = './model_2.pth'
+output_path = './model_6.pth'
 torch.save(net.state_dict(), output_path)
 
 print("DONE TRAINING")
